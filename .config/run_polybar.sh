a4633def926a562e3219f10cc3a17b0bec6ba04c
AUTplayed <fips.hem@gmail.com>
2017-11-22 10:57:40 +0100

    added polybar

---
+++ .config/run_polybar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    export MONITOR=$m && echo $MONITOR && polybar -q --reload example & >/dev/null >&/dev/null
+  done
+else
+  polybar --reload example &
+fi
