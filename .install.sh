487612c287fb0d453e1310118dce40e8e6d2b935
AUTplayed <fips.hem@gmail.com>
2018-02-25 20:12:14 +0100

    added install script

---
+++ .install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	dest=~
+else
+	dest="$1"
+fi
+
+copyover() {
+	\cp -rf "$1" "$dest"
+}
+
+shopt -s dotglob
+me=$(basename $0)
+for f in *; do
+	if [[ ! $f =~ $me.* ]]; then
+		echo "$f"
+		copyover "$f"
+	fi
+done
+
+
