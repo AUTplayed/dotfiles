1b580a6aadfe7e9ea0b3a7fd4b261878f5208e03
Philipp Hemmelmayr <philipp.hemmelmayr@dynatrace.com>
2019-09-26 14:38:34 +0200

    added screen layout

---
+++ .layouts/layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 2560x1440 --pos 5120x0 --rotate normal --output DP1 --off --output DP1-1 --off --output DP1-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP1-3 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
